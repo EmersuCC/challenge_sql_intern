e69199b931c05906fe60a84cba3985609088a510
emersucc <emersonbarbosa@cc.ci.ufpb.br>
2023-02-15 00:16:45 -0300

    Atualização dos scripts da tabela.

--- Schema/Scripts/Tables/Cliente.sql
+++ Schema/Scripts/Tables/Cliente.sql
@@ -1,20 +1,9 @@
-CREATE TABLE IF NOT EXISTS CLIENTE (
-    ID_CLIENTE SERIAL PRIMARY KEY,
-    NOME VARCHAR ( 50 ) NOT NULL,
-    IDADE INTEGER NOT NULL,
-    STATUS_BLOQUEIO CHAR(1) NOT NULL DEFAULT 'A',
-    LIMITE_CREDITO DECIMAL(10, 2) NOT NULL
-);
+-- Criação da tabela CLIENTE
 
-INSERT INTO CLIENTE (NOME, IDADE, STATUS_BLOQUEIO, LIMITE_CREDITO)
-VALUES 
-	('Emerson Santos Barbosa', 18, 'A', 1500),
-	('Ester R.L. Santos', 18, 'A', 2500),
-	('Elainy', 37, 'A', 5500),
-	('Ednaldo', 60, 'B', 1500),
-	('Maria do Socorro', '57', 'A', 8500),
-	('Keyla Silva', 21, 'B', 500),
-	('Mércia Silva', 20, 'A', 1000),
-	('Eliseu R.L. Santos', 21, 'A', 550),
-	('Juliana Cabral', 31, 'A', 15000),
-	('Maicosuel Lurdes', 19, 'A', 10);
+CREATE TABLE IF NOT EXISTS CLIENTE (
+	ID_CLIENTE SERIAL PRIMARY KEY,
+	NOME VARCHAR ( 50 ) NOT NULL,
+	IDADE INTEGER NOT NULL,
+	STATUS_BLOQUEIO CHAR(1) NOT NULL DEFAULT 'A',
+	LIMITE_CREDITO DECIMAL(10, 2) NOT NULL
+);
