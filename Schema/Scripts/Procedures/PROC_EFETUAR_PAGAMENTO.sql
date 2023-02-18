3c5baf235c7d3f9f9407ab9535182d9ab990f41b
emersucc <emersonbarbosa@cc.ci.ufpb.br>
2023-02-18 02:04:12 -0300

    Prrocedure para efetuação de pagamentos

---
+++ Schema/Scripts/Procedures/PROC_EFETUAR_PAGAMENTO.sql
@@ -0,0 +1,51 @@
+CREATE OR REPLACE PROCEDURE EFETUAR_PAGAMENTO( 
+    ID_FAT INTEGER,
+    VALOR_PAGO DECIMAL(10, 2)
+) LANGUAGE PLPGSQL AS $$
+DECLARE 
+    VALOR_FATURA DECIMAL(10, 2);
+    DATA_PAGAMENTO TIMESTAMP;
+BEGIN
+    DATA_PAGAMENTO := NOW();
+    
+    IF (
+        SELECT STATUS
+        FROM FATURA
+        WHERE ID_FATURA = ID_FAT
+    ) = 'A' THEN
+        
+        SELECT VALOR INTO VALOR_FATURA
+        FROM FATURA
+        WHERE ID_FATURA = ID_FAT;
+        
+        IF VALOR_FATURA <= VALOR_PAGO THEN
+            
+            UPDATE FATURA
+            SET STATUS = 'P'
+            WHERE ID_FATURA = ID_FAT;
+            
+            INSERT INTO PAGAMENTO (
+                DATA_PAGAMENTO,
+                VALOR,
+                ID_FATURA
+            ) VALUES (
+                DATA_PAGAMENTO,
+                VALOR_PAGO,
+                ID_FAT
+            );
+            
+            RAISE NOTICE 'Pagamento efetuado com sucesso. Troco: %', VALOR_PAGO - VALOR_FATURA;
+            
+        ELSE
+            RAISE NOTICE 'Valor pago é menor do que o valor da fatura';
+        END IF;
+        
+    ELSE
+        RAISE NOTICE 'A fatura já encontra-se paga';
+    END IF;
+    
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RAISE NOTICE 'Fatura não encontrada.';
+END;
+$$;
